04c57201bb6f35f5df8d4845970409b08a4a9c26
diya <kalamkardiya@gmail.com>
2026-08-15 20:47:55 +0530

    Delete .history directory

--- .history/src/data_loader_20260812214943.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from . import config as cfg
-
-def load_raw():
-    cat = pd.read_csv(cfg.CATEGORY_MAP_FILE)
-    pay = pd.read_csv(cfg.PAYMENT_MAP_FILE)
-    cust = pd.read_csv(cfg.CUSTOMER_FILE)
-    txn = pd.read_csv(cfg.TXN_FILE)
-    return cat, pay, cust, txn
